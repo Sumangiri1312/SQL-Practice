724360f1c0962d641350b009176d6d4c050e5176
Sumangiri1312 <sumangiri1718@gmail.com>
2026-09-03 18:19:16 +0545

    SQL

--- sql_learningg day5(subqueries).sql
+++ sql_learningg day5(subqueries).sql
@@ -114,7 +114,7 @@ select r_name, avg_rating from(select r_id, avg(restaurant_rating) as 'avg_ratin
 									group by r_id) t1 join restaurants t2 on t1.r_id = t2.r_id;
                                     
                                     
--- Usage with FROM 
+-- Usage with Having 
 -- Find genres having avg score > avg score of all the movies
 select genre, avg(score) from movies
 group by genre
