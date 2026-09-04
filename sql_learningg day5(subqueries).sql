486c3edddc2f18799ad963973df48aa7db537042
Sumangiri1312 <sumangiri1718@gmail.com>
2026-09-04 14:06:23 +0545

    SQL

--- sql_learningg day5(subqueries).sql
+++ sql_learningg day5(subqueries).sql
@@ -143,4 +143,39 @@ WHERE user_id IN (
 
 
 
+-- OTHER PRACTICED QUESTIONS
+
+-- Display the names of athletes who won a gold medal in the 2008 Olympics and whose height is greater
+-- than the average height of all athletes in the 2008 Olympics.
+SELECT Name FROM olympi
+WHERE Year= 2008 AND Medal='Gold' AND Height > (SELECT AVG(Height) FROM olympi
+													WHERE Year= 2008);
+                                                    
+-- Display the names of athletes who won a medal in the sport of basketball in the 2016 Olympics
+-- and whose weight is less than the average weight of all athletes who won a medal in the 2016 Olympics.
+SELECT Name FROM olympi
+WHERE Sport = 'Basketball'AND Year= 2016 AND Medal IS NOT NULL AND Weight < (SELECT AVG(Weight) FROM olympi
+					WHERE Year= 2016 AND Medal IS NOT NULL);
+                    
+-- Display the names of all athletes who have won a medal in the sport of swimming in both the 2008 and 2016 Olympics.
+SELECT DISTINCT Name FROM olympi
+WHERE Medal IS NOT NULL AND Sport = 'Swimming' AND Year= 2016 AND Name IN (SELECT Name FROM olympi
+												WHERE Medal IS NOT NULL AND Sport = 'Swimming' AND Year= 2008);
+                                                
+-- Display the names of all CITY that have won more than 50 medals in a single year.
+SELECT City
+FROM olympi
+WHERE Medal IS NOT NULL
+GROUP BY Year, City
+HAVING COUNT(*) > 50;
+
+-- Display the names of all athletes who have won medals in more than one sport in the same year.
+SELECT Name, Year
+FROM olympi
+WHERE Medal IS NOT NULL
+GROUP BY Name, year
+HAVING COUNT(DISTINCT Sport) > 1;
+
+
+
 
