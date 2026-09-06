478f6893bc63976b29c2401155cab7e49d9d63f5
Sumangiri1312 <sumangiri1718@gmail.com>
2026-09-06 10:45:16 +0545

    SQL

--- sql_learning_day6(window-function).sql
+++ sql_learning_day6(window-function).sql
@@ -114,9 +114,117 @@ GROUP BY MONTH(date), MONTHNAME(date);
 
 
 -- SOME OTHER WINDOW FUNCTIONS
+-- RANK()
 -- top 5 batsman from each team
 SELECT * FROM (SELECT batter, BattingTeam, SUM(batsman_run),RANK() OVER(PARTITION BY BattingTeam ORDER BY SUM(batsman_run) DESC) AS 'rank' FROM ipl
 GROUP BY batter, BattingTeam) t
 WHERE t.rank<6
+
+
+-- CUMULATIVE SUM
+SELECT * FROM (SELECT CONCAT('Match-', ROW_NUMBER() OVER(ORDER BY ID)) AS 'Match_no', 
+SUM(batsman_run) AS 'run_scored',
+SUM(SUM(batsman_run)) OVER(ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS 'career_run'
+FROM ipl
+WHERE batter= 'V Kohli'
+GROUP BY ID) t
+WHERE t.Match_no = 'Match-50' OR t.Match_no = 'Match-100' OR t.Match_no = 'Match-200';
+
+-- CUMULATIVE AVERAGE
+SELECT CONCAT('Match-', ROW_NUMBER() OVER(ORDER BY ID)) AS 'match-no' ,
+SUM(batsman_run) AS 'run-scored',
+SUM(SUM(batsman_run)) OVER w AS 'career_run',
+AVG(SUM(batsman_run)) OVER w 'career_avg_run'
+FROM ipl
+WHERE batter= 'V Kohli'
+GROUP BY ID
+WINDOW w AS (ORDER BY ID ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW);
+
+-- RUNNING AVERAGE
+SELECT CONCAT('Match-', ROW_NUMBER() OVER(ORDER BY ID)) AS 'match-no' ,
+SUM(batsman_run) AS 'run-scored',
+SUM(SUM(batsman_run)) OVER w AS 'career_run',
+AVG(SUM(batsman_run)) OVER w 'career_avg_run',
+AVG(SUM(batsman_run)) OVER(ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS 'rolling_avg'
+FROM ipl
+WHERE batter= 'V Kohli'
+GROUP BY ID
+WINDOW w AS (ORDER BY ID ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW);
+
+-- PERCENT OF TOTAL
+SELECT f_name,
+(total_value/SUM(total_value) OVER())*100 AS 'percent_of_total'
+FROM (SELECT f_id,SUM(amount) AS 'total_value' FROM orders1 t1
+JOIN order_details1 t2
+ON t1.order_id = t2.order_id
+WHERE r_id = 1
+GROUP BY f_id) t
+JOIN food t3
+ON t.f_id = t3.f_id
+ORDER BY (total_value/SUM(total_value) OVER())*100 DESC;
+
+
+-- PERCENT CHANGE
+-- Percent Change = ((new value - old value)/old value)/100
+SELECT YEAR(Date),MONTHNAME(Date),SUM(views) AS 'views',
+((SUM(views) - LAG(SUM(views)) OVER(ORDER BY YEAR(Date),MONTH(Date)))/LAG(SUM(views)) OVER(ORDER BY YEAR(Date),MONTH(Date)))*100 AS 'Percent_change'
+FROM youtube_views
+GROUP BY YEAR(Date),MONTH(Date)
+ORDER BY YEAR(Date),MONTH(Date);
+
+SELECT *,
+((Views - LAG(Views,7) OVER(ORDER BY Date))/LAG(Views,7) OVER(ORDER BY Date))*100 AS 'weekly_percent_change'
+FROM youtube_views;
+
+
+-- Percentiles & Quantiles
+SELECT *,
+PERCENTILE_DISC(0.5) WITHIN GROUP(ORDER BY marks) OVER(PARTITION BY branch) AS 'median_marks',
+PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY marks) OVER(PARTITION BY branch) AS 'median_marks_cont'
+FROM marks; 
+-- current MySQL version does not support PERCENTILE_DISC() or PERCENTILE_CONT() but work in other database
+
+
+SELECT * FROM (SELECT *,
+PERCENTILE_CONT(0.25) WITHIN GROUP(ORDER BY marks) OVER() AS 'Q1',
+PERCENTILE_CONT(0.75) WITHIN GROUP(ORDER BY marks) OVER() AS 'Q3'
+FROM marks) t
+WHERE t.marks <= t.Q1 - (1.5*(t.Q3 - t.Q1)) AND t.marks >= t.Q3 + (1.5*(t.Q3 - t.Q1));
+-- to filter outliers values
+
+
+-- SEHMENTATION: 
+	-- Segmentation using NTILE is a technique in SQL for dividing a dataset into equal-
+	-- sized groups based on some criteria or conditions, and then performing
+	-- calculations or analysis on each group separately using window functions.
+SELECT *,
+NTILE(3) OVER(ORDER BY marks DESC) AS 'buckets'
+FROM marks;
+
+SELECT brand_name,model,price, 
+CASE 
+	WHEN bucket = 1 THEN 'budget'
+    WHEN bucket = 2 THEN 'mid-range'
+    WHEN bucket = 3 THEN 'premium'
+END AS 'phone_type'
+FROM (SELECT brand_name,model,price,
+NTILE(3) OVER(PARTITION BY brand_name ORDER BY price) AS 'bucket' 
+FROM smartphones) t;
+
+
+-- Cumulative Distribution
+-- CUME_DIST(): Its main use is to find what proportion/percentage of rows have a value less than or equal to the current row's value.
+SELECT * FROM (SELECT *,
+CUME_DIST() OVER(ORDER BY marks) AS 'Percentile_Score'
+FROM marks) t
+WHERE t.Percentile_Score > 0.90;
+
+
+-- Partition By multiple columns
+SELECT * FROM (SELECT source,destination,airline,AVG(price) AS 'avg_fare',
+DENSE_RANK() OVER(PARTITION BY source,destination ORDER BY AVG(price)) AS 'rank'
+FROM flights
+GROUP BY source,destination,airline) t
+WHERE t.rank < 2
 
 
